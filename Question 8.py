d14b8fdc3587a4ff6bd425ccee0c898a785abfd5
Krishna Tanwar <142567194+KrishnaTanwar@users.noreply.github.com>
2023-08-31 12:59:10 +0530

    Delete Question 8.py

--- Question 8.py
+++
@@ -1,7 +0,0 @@
-# Ques8: Area & circumference of a circle by user input
-import math
-r = int(input('Enter the radius of circle: '))
-area = math.pi*r**2
-circum = 2*math.pi*r
-print("The Area of Circle is: ",area)
-print("The circumference of circle is: ",circum)
