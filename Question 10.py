4c98599c40677ec6596bac06ed23313042d95deb
Krishna Tanwar <142567194+KrishnaTanwar@users.noreply.github.com>
2023-08-31 12:57:48 +0530

    Delete Question 10.py

--- Question 10.py
+++
@@ -1,5 +0,0 @@
-# Ques 10: Swapping two numbers entered by the user.
-a = int(input('Enter your First Number: '))
-b = int(input('Enter your Second Number: '))
-a,b = b,a
-print("The swapping numbers are: ",a,b)
