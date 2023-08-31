e3fe0b202313ab5476f7511a1b8cf7c457b55cad
Krishna Tanwar <142567194+KrishnaTanwar@users.noreply.github.com>
2023-08-31 12:59:35 +0530

    Delete Question 6.py

--- Question 6.py
+++
@@ -1,5 +0,0 @@
-#Ques 6: Program to input two integer values and calculate the first number raised to the power second number.
-a = int(input('Enter your First Number:'))
-b = int(input('Enter your Second Number:'))
-power = a**b
-print('The number raised to the power of second number is: ',power)
