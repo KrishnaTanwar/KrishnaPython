b29aa5f64c70c66d6104a019a2b8014e64224854
Krishna Tanwar <142567194+KrishnaTanwar@users.noreply.github.com>
2023-08-31 12:59:43 +0530

    Delete Question 7.py

--- Question 7.py
+++
@@ -1,7 +0,0 @@
-# Ques7: Area & Perimeter of Rectangle with user input
-a = int(input('Enter the length of rectangle: '))
-b = int(input('Enter the breadth of rectangle: '))
-area = a*b
-perimeter = 2*(a+b)
-print('The Area of the rectangle is:', area)
-print('The Perimeter of the rectangle is:', perimeter)
