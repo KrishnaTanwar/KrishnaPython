abcd3cb92689f04a7bc3be51f037bbc5ced5f9e4
Krishna Tanwar <142567194+KrishnaTanwar@users.noreply.github.com>
2023-08-31 12:59:19 +0530

    Delete Question 4.py

--- Question 4.py
+++
@@ -1,8 +0,0 @@
-# Ques 4: Subtract 2 entered integers
-a = int(input("Enter 1st integer value: "))
-b = int(input("Enter 2nd integer value: "))
-if a>b:
-    print("The subtraction of two entered values is:",a-b)
-else:
-    print("The subtraction of two entered values is:",b-a)    
-
