f70a7d37c9186f8c2d7ad30a27a31a2f60e148cb
Krishna Tanwar <142567194+KrishnaTanwar@users.noreply.github.com>
2023-09-03 02:55:38 -0700

    Question 1

---
+++ Practice Questions/Question 1.py
@@ -0,0 +1,12 @@
+# Question 1: Program to find whether an input number is even or odd.
+
+# Input Section
+
+a = int(input("Enter a number:"))
+
+# Logic Section
+
+if a%2==0:
+    print(a,"is a an Even number.")
+else:
+    print(a,'is an Odd number.')
