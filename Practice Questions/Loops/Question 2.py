184ccadf2a409925f6fac640488e9fa776e42e38
Krishna Tanwar <142567194+KrishnaTanwar@users.noreply.github.com>
2023-09-04 05:59:55 +0530

    Factorial of a Number

---
+++ Practice Questions/Loops/Question 2.py
@@ -0,0 +1,15 @@
+#Question 2: Program to find the factorial of an entered number.
+
+#Input Section
+import math
+
+a = int(input("Enter a number:"))
+
+# Logic & Output Section
+
+if a<0:
+    print("Please enter a number greater than or equal to ZERO.")
+elif a==0:
+    print("Factorial of Zero (0!) is 1.")
+else:
+    print("Factorial of",a,"is",math.factorial(a))
