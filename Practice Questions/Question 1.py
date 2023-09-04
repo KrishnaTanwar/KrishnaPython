474f30c60db1d1f97cc14d7627c7ab36d7ba5735
Krishna Tanwar <142567194+KrishnaTanwar@users.noreply.github.com>
2023-09-04 05:58:27 +0530

    PRIME NUMBER PYTHON

---
+++ Practice Questions/Question 1.py
@@ -0,0 +1,16 @@
+# Question 1 : Program to find whether an entered number is prime or not.
+
+# Input Section
+
+a = int(input("Enter a number: "))
+y = int(a/2)
+
+# Logic Section
+
+for x in (1,y):
+    lg1 = a%x
+if lg1==0:
+    print(a," is NOT a Prime number.")
+else:
+    print(a,"is a Prime Number.")
+
