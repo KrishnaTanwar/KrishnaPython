6e46596cb0b65f092111997e72c669678429aba5
Krishna Tanwar <142567194+KrishnaTanwar@users.noreply.github.com>
2023-09-05 06:39:10 +0530

    Sum of n Natural no.

---
+++ Practice Questions/Question 1 (sum of n natural no).py
@@ -0,0 +1,14 @@
+# Q1: Program to calculate sum of first N natural numbers.
+
+# Input Section
+
+a = int(input("Enter the number: "))
+
+# Logic Section
+
+b = a
+lg = int(b*(b+1)/2)
+
+# Output Section
+
+print("The sum of first n natural no. before",a,'is',lg)
