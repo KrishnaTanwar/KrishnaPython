fc843041aa79439922bd0a4f0625086779870616
Krishna Tanwar <142567194+KrishnaTanwar@users.noreply.github.com>
2023-12-28 11:52:28 +0530

    #4eligibilitytovote
    Q4. Write a program that takes a user's age as input and determines if they are eligible to vote
    (above 18)

---
+++ Basic Questons/#4eligibilitytovote.py
@@ -0,0 +1,16 @@
+# Python Program to check eligible candidates for vote
+
+# INPUT SECTION
+
+age = int(input("Enter your age: ")) #18
+
+# LOGIC SECTION
+
+if age >= 18:
+    result = "You are eligible to vote!"
+else:
+    result = "Sorry, you are not eligible to vote."
+
+# OUTPUT SECTION
+
+print(result)
